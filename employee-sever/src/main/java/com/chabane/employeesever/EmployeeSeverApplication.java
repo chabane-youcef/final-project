2e8c07aa86b25dbfc20a4e2698d095164e6f7bf8
Chabane Youcef El Amine <joechabane98@gmail.com>
2021-06-30 19:23:22 +0200

    created employee-server

---
+++ employee-sever/src/main/java/com/chabane/employeesever/EmployeeSeverApplication.java
@@ -0,0 +1,13 @@
+package com.chabane.employeesever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSeverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSeverApplication.class, args);
+    }
+
+}
---
+++ employee-sever/src/test/java/com/chabane/employeesever/EmployeeSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chabane.employeesever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSeverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
