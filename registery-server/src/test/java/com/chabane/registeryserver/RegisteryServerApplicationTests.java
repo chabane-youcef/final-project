b7733baa7dd3a118a9a729bb72c558bd10eb4b05
Chabane Youcef El Amine <joechabane98@gmail.com>
2021-06-30 05:36:12 +0200

    added discovery service

---
+++ registery-server/src/test/java/com/chabane/registeryserver/RegisteryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chabane.registeryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisteryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
